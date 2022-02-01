7f27525b2e4196325b27f6ef269aae38fb52d288
Chirikru <98220560+Chirikru@users.noreply.github.com>
2022-02-01 21:16:58 +0300

    Update alertmanager-configure-pagertree.sh

--- util/alertmanager-configure-pagertree.sh
+++ util/alertmanager-configure-pagertree.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 script=$0
 
-sed -i "s|https://api.telegram.org/bot5209561422:AAEEPNrU6AgO2IxBTykaehhonAQ4E6M37NY/sendmessage?chat_id=539686084&text=%22%D0%9E%D0%A8%D0%98%D0%91%D0%9A%D0%90%205XX%22|$1|" $(dirname "$script")/../alertmanager/config.yml
+sed -i "s|https://api.telegram.org/bot"АпиТелеграмма без кавычек"/sendmessage?chat_id="ID чата без кавычек"&text=%22AHTUNG%20Site%20DOWN%22|$1|" $(dirname "$script")/../alertmanager/config.yml
